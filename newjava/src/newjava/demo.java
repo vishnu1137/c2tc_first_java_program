5cad25c9cc4f886991e412792d3ffc4d60ab7b42
vishnu <vishnu@DESKTOP-98893TF>
2022-12-01 09:10:59 +0530

    first commit

---
+++ newjava/src/newjava/demo.java
@@ -0,0 +1,16 @@
+package newjava;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		float x=555.6f;
+		int y=(int)x;
+		System.out.println(x);
+		System.out.println(y);
+		String text = "this is the sample text for located string";
+		System.out.println("the location of located text : "+ text.indexOf("located"));
+	}
+
+}
