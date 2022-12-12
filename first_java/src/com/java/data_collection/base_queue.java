236e8c7e5276430ef112ab98937597756ee6dee0
vishnu <vishnu@DESKTOP-98893TF>
2022-12-12 23:35:35 +0530

    basic queue program

--- first_java/src/com/java/data_collection/base_queue.java
+++ first_java/src/com/java/data_collection/base_queue.java
@@ -11,19 +11,22 @@ public class base_queue {
 			Iq.add(i);
 		
 		}
-		System.out.println("The value is "+Iq+"\n");
+		System.out.println("The value is "+Iq+"\n"); //The value is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
 		Iq.remove(5);
-		System.out.println("Using remove method: "+Iq);
+		System.out.println("Using remove method: "+Iq);//Using remove method: [1, 2, 3, 4, 10, 6, 7, 8, 9, 11]
 		Iq.element();
-		System.out.println("using element method "+Iq+"\n");
+		System.out.println("using element method "+Iq+"\n");//using element method [1, 2, 3, 4, 10, 6, 7, 8, 9, 11]
 		Iq.offer(3);
-		System.out.println("using offer method "+Iq+"\n");
+		System.out.println("using offer method "+Iq+"\n");//using offer method [1, 2, 3, 4, 3, 6, 7, 8, 9, 11, 10]
 		Iq.poll();
-		System.out.println("Using poll method "+Iq+"\n");
+		System.out.println("Using poll method "+Iq+"\n");//Using poll method [2, 3, 3, 4, 10, 6, 7, 8, 9, 11]
+
 		
-		System.out.println("The size is "+Iq.size()+"\n");
+		System.out.println("The size is "+Iq.size()+"\n");//The size is 10
 		Iq.clear();
-		System.out.println("The value is "+Iq+"\n");
+		System.out.println("The value is "+Iq+"\n");//The value is []
+
 		
 		
 	}
